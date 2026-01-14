93634e0793f63d693d44b60a2213e34ee2801865
sid-dev-99 <engrsiddharth1@gmail.com>
2026-01-14 22:42:45 +0530

    chore: update project configuration

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'i1.sndcdn.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'i.scdn.co',
+            },
         ],
     },
 };
